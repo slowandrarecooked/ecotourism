178219ee34c9ee2309796f835d764662a6ee9a96
slowandrarecooked <airbendervineeth@gmail.com>
2023-08-10 21:48:24 +0530

    responsive navbar

--- ecotourismapp/src/Components/Navbar.jsx
+++ ecotourismapp/src/Components/Navbar.jsx
@@ -1,63 +1,35 @@
-import { Link } from "react-router-dom";
-export const Navbar = () => {
-  const Links = [
-    { path: "/", title: "Home" },
-    { path: "/Destinations", title: "Destinations" },
-    { path: "/AboutUs", title: "About us" },
-  ];
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css";
+
+function Navbar() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
   return (
-    <div
-      className="Navbar"
-      style={{
-        display: "flex",
-        justifyContent: "space-evenly",
-        padding: "20px",
-        backgroundColor: "#7DBBA9",
-      }}
-    >
-      <div className="logo">
-        <img src="https://i.ibb.co/kJTF94R/logo.png" alt="" />
-      </div>
-      <div
-        className="links"
-        style={{
-          display: "flex",
-          gap: "60px",
-          margin: "auto",
-          fontSize: "20px",
-        }}
-      >
-        {Links.map((link) => (
-          <Link
-            to={link.path}
-            key={link.path}
-            style={{ textDecoration: "none", color: "white" }}
-          >
-            {link.title}
-          </Link>
-        ))}
-      </div>
-      <div
-        className="book"
-        style={{
-          display: "flex",
-          marginTop: "20px",
-        }}
-      >
-        <button
-          style={{
-            border: "none",
-            borderRadius: "20px",
-            height: "50px",
-            backgroundColor: "yellow",
-            padding: "0px 20px 0px 20px",
-            backgroundColor: "#FACE55",
-            fontWeight: "bold",
-          }}
-        >
-          Book Now
+    <header>
+      <h3>
+        <img src="https://i.ibb.co/kJTF94R/logo.png" alt="logo" border="0" />
+      </h3>
+      <nav ref={navRef}>
+        <a href="/#">Home</a>
+        <a href="/#">Destinations</a>
+        <a href="/#">Activites</a>
+        <a href="/#">Packages</a>
+        <a href="/#">About</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
         </button>
-      </div>
-    </div>
+      </nav>
+      <button className="book-btn">Book Now</button>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
   );
-};
+}
+
+export default Navbar;
