d4b1ef1d1aaa9d0a8c6080d67e5a3e3e7a856cf9
Swetarani Patel <sweta@Swetaranis-MacBook-Air.local>
2023-08-10 19:49:28 +0530

    paymentpagebasicstructure

--- ecotourismapp/src/App.js
+++ ecotourismapp/src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Payment from "./Components/Payment";
 
 function App() {
   return (
     <div className="App">
       <h1>Ecotourism</h1>
+      <Payment/>
     </div>
   );
 }
