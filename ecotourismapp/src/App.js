67726a76b407690ac03d454e332fdb2e392fa9b0
slowandrarecooked <airbendervineeth@gmail.com>
2023-08-15 20:23:20 +0530

    v2

--- ecotourismapp/src/App.js
+++ ecotourismapp/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import { AllRoutes } from "./Routes/AllRoutes";
 import { Footer } from "./Components/Footer";
+import { Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
--- ecotourismapp/src/Routes/AllRoutes.jsx
+++ ecotourismapp/src/Routes/AllRoutes.jsx
@@ -4,7 +4,7 @@ import { ProductDetails } from "../Components/ProductDetails";
 export const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/productDetails" element={<ProductDetails />} />
+      <Route path="/products/:id" element={<ProductDetails />} />
     </Routes>
   );
 };
