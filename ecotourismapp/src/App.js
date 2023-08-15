3596b8a2ed0d43411b61e6c730da121470e522fe
Swetarani Patel <sweta@Swetaranis-MacBook-Air.local>
2023-08-15 21:07:44 +0530

    payy

--- ecotourismapp/src/App.js
+++ ecotourismapp/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-import Payment from "./Components/Payment/Payment";
-
+import Navbar from "./Components/Navbar";
+import { AllRoutes } from "./Routes/AllRoutes";
+import { Footer } from "./Components/Footer";
+import { Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-    <Payment/>
+      <Navbar />
+      <AllRoutes />
+      <Footer />
     </div>
   );
 }
