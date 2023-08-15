6a02559e6b0ec44d2b40f81d2e5085859cf87ee3
Swetarani Patel <sweta@Swetaranis-MacBook-Air.local>
2023-08-15 20:24:33 +0530

    paymnet

--- ecotourismapp/src/App.js
+++ ecotourismapp/src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import AllRoutes from "./Components/AllRoutes";
 import Payment from "./Components/Payment/Payment";
 
 function App() {
   return (
     <div className="App">
-    <AllRoutes/>
+    <Payment/>
     </div>
   );
 }
