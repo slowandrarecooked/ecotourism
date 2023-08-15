8f4963e518bbb2f14e0be1e781f751da10852da8
saurabhWakde <128630330+saurabhWakde@users.noreply.github.com>
2023-08-15 20:53:58 +0530

    done

--- ecotourismapp/src/App.js
+++ ecotourismapp/src/App.js
@@ -1,6 +1,16 @@
 import "./App.css";
+import Navbar from "./Components/Navbar";
+import { AllRoutes } from "./Routes/AllRoutes";
+import { Footer } from "./Components/Footer";
+import { Link } from "react-router-dom";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <AllRoutes />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
